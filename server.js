01b2747bee1e7016e43d8b39346773d2e56ddcb9
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-09-29 06:55:23 +0900

    ユーザー新規登録API
    パスワードがそのままなので脆弱性

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
+const { PrismaClient } = require('@prisma/client')
 const express = require('express')
 const app = express()
 
 const PORT = 8000
+
+const prisma = new PrismaClient()
+
+// 新規ユーザー登録API
+app.post('/api/auth/register', async (req, res) => {
+  const { username, email, password } = req.body
+  const user = await prisma.user.create({
+    data: {
+      username,
+      email,
+      password,
+    },
+  })
+
+  return res.json({ user })
+})
 
 app.get('/', (req, res) => {
   console.log(req)
