5365bf33f0fa9cf5dd97c247a132ddc2271a3e99
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-10-05 07:03:21 +0900

    CORSエラー解消

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const app = express()
 const authRoute = require('./routers/auth')
+const cors = require('cors')
+
 require('dotenv').config()
 
 const PORT = 8000
 
 app.use(express.json())
-
+app.use(cors())
 app.use('/api/auth', authRoute)
 
 app.listen(PORT, () => console.log(`server is running on Port ${PORT}`))
