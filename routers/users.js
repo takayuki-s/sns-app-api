cdc3c5ab15ac6f4ac9338248101466e5ae4bd22b
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-10-17 07:46:50 +0900

    API取得修正

--- routers/users.js
+++ routers/users.js
@@ -9,12 +9,11 @@ router.get('/find', isAuthenticated, async (req, res) => {
     if (!user) {
       res.status(404).json({ error: 'ユーザーが見つかりませんでした' })
     }
-    res
-      .status(200)
-      .json({
-        user: { id: user.id, email: user.email, username: user.username },
-      })
+    res.status(200).json({
+      user: { id: user.id, email: user.email, username: user.username },
+    })
   } catch (err) {
     res.status(500).json({ error: err.message })
   }
 })
+module.exports = router
