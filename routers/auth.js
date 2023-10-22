937a685c9c1b8f207b45d840f709673d01b57394
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-10-23 06:44:01 +0900

    アイコン設定

--- routers/auth.js
+++ routers/auth.js
@@ -2,12 +2,14 @@ const router = require('express').Router()
 const { PrismaClient } = require('@prisma/client')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const generateIcon = require('../utils/generateIdenticon')
 
 const prisma = new PrismaClient()
 
 // 新規ユーザー登録API
 router.post('/register', async (req, res) => {
   const { username, email, password } = req.body
+  const defaultIconImage = generateIcon(email)
   const hashedPassword = await bcrypt.hash(password, 10)
   const user = await prisma.user.create({
     data: {
@@ -17,9 +19,12 @@ router.post('/register', async (req, res) => {
       profile: {
         create: {
           bio: 'はじめまして',
-          profileImageUrl: 'sample.png',
+          profileImageUrl: defaultIconImage,
         },
       },
+    },
+    include: {
+      profile: true,
     },
   })
   return res.json({ user })
--- routers/posts.js
+++ routers/posts.js
@@ -18,7 +18,11 @@ router.post('/post', isAuthenticated, async (req, res) => {
         authorId: req.userId,
       },
       include: {
-        author: true,
+        author: {
+          include: {
+            profile: true,
+          },
+        },
       },
     })
     return res.status(200).json(newPost)
@@ -36,7 +40,11 @@ router.get('/get_latest_post', async (req, res) => {
       take: 10,
       orderBy: { createdAt: 'desc' },
       include: {
-        author: true,
+        author: {
+          include: {
+            profile: true,
+          },
+        },
       },
     })
     return res.json(latestPosts)
